5f838b1792b26329a1b18e4266a9baba0cc31b58
Saket Sinha <103985771+saketksinha@users.noreply.github.com>
2023-08-03 19:44:13 +0530

    Revert "Update Main.java"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,11 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) {
-
-        
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new Your Bot Username());
         } catch (TelegramApiException e) {
             e.printStackTrace();
-
-            
         }
     }
 }
