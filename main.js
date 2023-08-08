7bc2e0aac17397b01c2d7ae2b07d08b6e939a1ae
Saket Sinha <103985771+saketksinha@users.noreply.github.com>
2023-08-08 10:51:31 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,60 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function main() {
+    try {
+        const token = core.getInput("github_token", { required: true })
+        const numbers = core.getInput("numbers")
+        const owner = core.getInput("owner")
+        const repository = core.getInput("repository")
+        const branches = core.getInput("branches")
+        const prefix = core.getInput("prefix")
+        const suffix = core.getInput("suffix")
+        const soft_fail = core.getInput("soft_fail")
+
+        const client = github.getOctokit(token)
+
+        let branchesToDelete = branches ? branches.split(",") : []
+
+        if (numbers) {
+            for (const number of numbers.split(",")) {
+                const pull = await client.pulls.get({
+                    ...github.context.repo,
+                    pull_number: number
+                })
+                branchesToDelete.push(pull.data.head.ref)
+            }
+        }
+
+        let ownerOfRepository = owner ? owner : github.context.repo.owner
+        let repositoryContainingBranches = repository ? repository : github.context.repo.repo
+        
+        for (let branch of branchesToDelete) {
+            if (prefix)
+                branch = prefix + branch
+            if (suffix)
+                branch = branch + suffix
+            
+            console.log("==> Deleting \"" + ownerOfRepository + "/" + repositoryContainingBranches + "/" + branch + "\" branch")
+            
+            try {
+                await client.git.deleteRef({
+                    owner: ownerOfRepository,
+                    repo: repositoryContainingBranches,
+                    ref: "heads/" + branch
+                })
+            } catch (error) {
+                const shouldFailSoftly = (soft_fail === 'true');
+                
+                if(shouldFailSoftly)
+                    core.warning(error.message)
+                else
+                    core.setFailed(error.message)
+            }
+        }
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
