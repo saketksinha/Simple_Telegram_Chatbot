5202f0bc4125c4726d9e7ad36107c14221215110
Saket Sinha <103985771+saketksinha@users.noreply.github.com>
2023-08-23 15:08:36 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,6 @@ public class Main {
 
         
         try {
-
-
-            
-
             
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new Your Bot Username());
